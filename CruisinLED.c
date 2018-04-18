ef2f8b45ceaa40841c21065ef9e7a6aaf8faa4fb
Ian Waldrop <32575831+ianwaldrop06@users.noreply.github.com>
2018-04-18 17:29:39 -0400

    CruisinLEDSonar and Drive1000 not finished

--- CruisinLED.c
+++ CruisinLED.c
@@ -21,7 +21,7 @@ task main()
 		MotorValue = (127 * (SensorValue[smokinPot]) / 4096);
 		motor[drive] = MotorValue;
 
-		if (MotorValue >= 1 && MotorValue <= 45)
+		if (MotorValue >= 1S && MotorValue <= 45)
 		{
 			SensorValue[ledGreen] = 1;
 			SensorValue[ledYellow] = 0;
